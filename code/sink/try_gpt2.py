02f09833e88f5a7cc728c1f250dab6f1e1c2b9fa
Siddharth Saha <siddharth.saha@cred.club>
2024-10-08 17:38:49 +0530

    tryout code

--- code/sink/try_gpt2.py
+++ code/sink/try_gpt2.py
@@ -1,12 +1,15 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 import argparse
+import torch
 
 def main(prompt):
-    model_name = 'meta-llama/Meta-Llama-3-8B'
+    model_name = 'meta-llama/Meta-Llama-3-8B-Instruct'
 
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForCausalLM.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name, torch_dtype=torch.float16,
+    )
 
     if tokenizer.pad_token_id is None:
         tokenizer.pad_token_id = tokenizer.eos_token_id
@@ -31,9 +34,14 @@ def main(prompt):
     '''
     print(prompt)
 
+    device = 'mps' if torch.backends.mps.is_available() else 'cpu'
+    print(f'Device: {device}')
+
+    model = model.to(device)
+
     inputs = tokenizer(
         prompt, return_tensors='pt', padding=True, truncation=True,
-    )
+    ).to(device)
     
     tokens = model.generate(
         inputs['input_ids'],
@@ -42,6 +50,8 @@ def main(prompt):
         pad_token_id=tokenizer.pad_token_id,
         max_length=len(prompt) + 50,
     )
+
+    # skip input
 
     text = tokenizer.batch_decode(tokens)[0]
     print(text)
