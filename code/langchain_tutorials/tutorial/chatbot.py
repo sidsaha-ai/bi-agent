be2f5f454c9e4d154b829f9b7ac6140109956b16
Sid Saha <siddharthsaha@Siddharths-MacBook-Pro.local>
2024-10-27 18:18:40 +0530

    starting the chatbot tutorial

---
+++ code/langchain_tutorials/tutorial/chatbot.py
@@ -0,0 +1,75 @@
+"""
+This script implements a chatbot by following the Langchain tutorial.
+"""
+
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, AIMessage
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, MessagesState, StateGraph
+
+class ChatApp:
+    model: ChatOpenAI
+    workflow: StateGraph
+    memory: MemorySaver
+    app: any
+
+    def _init_model(self):
+        # make the model
+        base_url: str = 'http://localhost:1234/v1'
+        api_key: str = 'lm_studio'
+
+        self.model = ChatOpenAI(temperature=0.0, base_url=base_url, api_key=api_key)
+
+    def call_model(self, state: MessagesState) -> dict:
+        """
+        Calls the language model.
+        """
+        response = self.model.invoke(state['messages'])
+        return {
+            'messages': response,
+        }
+
+    def __init__(self):
+        # make the model
+        self._init_model()
+        # make memory
+        self.memory = MemorySaver()
+        
+        # define the workflow
+        self.workflow = StateGraph(state_schema=MessagesState)
+        self.workflow.add_edge(START, 'model')
+        self.workflow.add_node('model', self.call_model)
+
+        self.app = self.workflow.compile(checkpointer=self.memory)
+    
+    def run(self) -> None:
+        """
+        This runs the chatbot.
+        """
+        config: str = {'configurable': {'thread_id': '123456'}}
+
+        print('Enter your message (enter STOP to end) at the prompt.\n')
+        while True:
+            user_message: str = input('Your message: ')
+            user_message = user_message.strip()
+            
+            if user_message.lower() == 'stop':
+                break
+
+            input_messages = [HumanMessage(user_message)]
+            output = self.app.invoke(
+                {'messages': input_messages}, config,
+            )
+            print(f'AI Response: {output["messages"][-1].content}')
+
+
+def main() -> None:
+    """
+    The main method to start execution.
+    """
+    chat_app = ChatApp()
+    chat_app.run()
+
+
+if __name__ == '__main__':
+    main()
